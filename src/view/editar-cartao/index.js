fa9aa9a10708eba7d4bf37d5e18ab9f5fe96ed06
Vinícius de Lima Mendonça <vinicius.mendoncaa@gmail.com>
2022-05-31 20:31:38 -0300

    Criação da página editar-cartão

--- src/view/editar-cartao/index.js
+++ src/view/editar-cartao/index.js
@@ -1,11 +1,36 @@
 import React from "react";
+import './editar-cartao.css';
 
-function Login() {
+function EditarCartao() {
     return(
-        <h1>
-            Um conteúdo ae
-        </h1>
+        <main>
+        <div class="container-principal">
+            <span>Coleção - Objetos</span>
+
+            <div class="titulo">
+                <h3>Atualize os dados da frente e do verso do flashcard</h3>
+            </div>
+
+            <form action="cartoes.html" method="post">
+
+            <div class="cartao">
+                <span>Frente</span>
+                <div class="cartao-frente">
+                    <input id="frente" value="Nome"></input>
+                </div>
+                <div class="separacao"></div>
+                <span>Verso</span>
+                <div class="cartao-verso">
+                    <input id="verso" value="Nome"></input>
+                </div>
+            </div>
+
+            <button class="btn btn-default d-flex" id="salvar" onclick="salvarDados()">Salvar alterações</button>
+
+            </form>
+        </div>
+    </main>
     )
 }
 
-export default Login;
+export default EditarCartao;
