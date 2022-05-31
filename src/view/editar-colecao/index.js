51f275daa6d58d4bc28adcc41bbc536df2bdcaab
Vinícius de Lima Mendonça <vinicius.mendoncaa@gmail.com>
2022-05-31 20:33:58 -0300

    Criação da página editar-coleção

--- src/view/editar-colecao/index.js
+++ src/view/editar-colecao/index.js
@@ -1,11 +1,45 @@
 import React from "react";
+import './editar-colecao.css';
 
-function Login() {
+function EditarColecao() {
     return(
-        <h1>
-            Um conteúdo ae
-        </h1>
+        <div class="container-principal">
+
+        <span>Editar Coleção</span>
+
+        <div class="titulo">
+            <h3>Atualize os dados necessários da coleção</h3>
+        </div>
+        
+        <div class="input-group">
+
+            <div class="container-input">
+
+                <form action="home.html" method="PUT">
+                    <label for="nome">Nome coleção</label>
+                    <input class="form-control" type="text" id="nome" placeholder="Insira o nome da coleção" />
+                    <label for="descricao">Descrição</label>
+                    <textarea class="form-control" type="text" id="descricao"
+                        placeholder="Descreva os detalhes da coleção"></textarea>
+                    <label for="imagem">Imagem</label>
+
+                    <div class="input-group">
+                        <input type="file" id="imagem" onchange="mudarNome(this)" />
+                        <label class="custom-file-label" for="imagem">Escolha uma imagem para a coleção criada...</label>
+                    </div>
+
+                    <div class="botao">
+                        <button type="submit" class="btn mt-3" id="salvar" onclick="salvarDados()">Salvar Alterações</button>
+                    </div>
+
+                </form>
+
+            </div>
+
+        </div>
+
+    </div>
     )
 }
 
-export default Login;
+export default EditarColecao;
