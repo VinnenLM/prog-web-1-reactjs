4b8b276d181ab9e70c266ab3dab156f8091d02eb
Vinícius de Lima Mendonça <vinicius.mendoncaa@gmail.com>
2022-06-06 14:37:13 -0300

    Importação do firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5FxwXY3utSw9hl8bAoE0i3pzu3vCsql8",
+  authDomain: "mindbooster-acdc7.firebaseapp.com",
+  projectId: "mindbooster-acdc7",
+  storageBucket: "mindbooster-acdc7.appspot.com",
+  messagingSenderId: "693366132335",
+  appId: "1:693366132335:web:424684d337fc3bc5adc79a",
+  measurementId: "G-3K2GRVX0ZR"
+};
+
+export default firebase.initializeApp(firebaseConfig);
--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import './login.css';
 
+import "../../config/firebase";
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
 function Login() {
+
+    const [email, setEmail] = useState();
+    const [senha, setSenha] = useState();
+    const [tipo, setTipo] = useState();
+
+    function autenticar() {
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, senha)
+        .then((userCredential) => {
+            setTipo('ok');
+          })
+          .catch((error) => {
+            setTipo('erro')
+          });
+    }
+
     return (
         <main>
-            <div class="container-principal">
-                <div class="titulo">
+            <div className="container-principal">
+                <div className="titulo">
                     <h1>Login</h1>
                 </div>
-                <div class="input-group">
-                    <div class="container-principal">
+                <div className="input-group">
+                    <div className="container-principal">
                         <form action="/cadastro" method="post">
-                            <label for="email">Email</label>
-                            <input class="form-control" type="email" id="email" placeholder="Insira o seu e-mail" />
-                            <span id="emailInvalido" class="naoMostrar">E-mail inválido</span>
-                            <label for="senha">Senha</label>
-                            <input class="form-control" type="password" id="senha" />
-                            <div class="botao">
-                                <button id="entrar" class="btn mt-3">Entrar</button>
+                            <label htmlFor="email">Email</label>
+                            <input onChange={(evt) => setEmail(evt.target.value)} className="form-control" type="email" id="email" placeholder="Insira o seu e-mail" />
+                            <span id="emailInvalido" className="naoMostrar">E-mail inválido</span>
+                            <label htmlFor="senha">Senha</label>
+                            <input onChange={(evt) => setSenha(evt.target.value)} className="form-control" type="password" id="senha" />
+                            {tipo==='ok' && <span>Entrou</span>}
+                            {tipo==='erro' && <span>Não entrou</span>}
+                            <div className="botao">
+                                <button id="entrar" className="btn mt-3" type="button" onClick={autenticar}>Entrar</button>
                             </div>
                         </form>
                     </div>
