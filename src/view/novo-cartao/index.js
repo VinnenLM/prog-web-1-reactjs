0ad4953b1519d3b7c7d30d5ac671e647bbd85d33
Vinícius de Lima Mendonça <vinicius.mendoncaa@gmail.com>
2022-06-06 09:13:25 -0300

    Criação da página novo-cartão

--- src/view/novo-cartao/index.js
+++ src/view/novo-cartao/index.js
@@ -1,10 +1,31 @@
 import React from "react";
+import './novo-cartao.css'
 
 function Login() {
     return(
-        <h1>
-            Um conteúdo ae
-        </h1>
+        <main>
+        <div class="container-principal">
+            <span>Coleção - Objetos</span>
+
+            <div class="titulo">
+                <h3>Preencha os dados da frente e do verso do flashcard</h3>
+            </div>
+
+            <div class="cartao">
+                <span>Frente</span>
+                <div class="cartao-frente">
+                    <input value="Nome"></input>
+                </div>
+                <div class="separacao"></div>
+                <span>Verso</span>
+                <div class="cartao-verso">
+                    <input value="Nome"></input>
+                </div>
+            </div>
+
+            <button class="btn btn-default d-flex" id="salvar">Cadastrar</button>
+        </div>
+    </main>
     )
 }
 
