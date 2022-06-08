9d8f6fb2501d9aeb12ac90bd3a83afd0cc4177ff
Vinícius de Lima Mendonça <vinicius.mendoncaa@gmail.com>
2022-06-08 15:05:43 -0300

    Utilização do useSelector no navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,6 +1,8 @@
 import EstiloNavBar from './navbar.module.css';
 import Logo from '../../assets/img/logo.png';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 
 function NavBar() {
     return (
@@ -11,6 +13,9 @@ function NavBar() {
                     <span>QI Labs</span>
                 </div>
                 <div className={EstiloNavBar.opcoes}>
+
+                    {useSelector(state => state.usuarioLogado) > 0 ? null : null}
+
                     <Link to="/cadastro">Cadastre-se</Link>
                     <Link to="/login" className={EstiloNavBar.entrar}>Entrar</Link>
                 </div>
