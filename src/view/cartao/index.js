31d3b0d2fa79b15bc1159443cba11defbcda24b1
Vinícius de Lima Mendonça <vinicius.mendoncaa@gmail.com>
2022-06-20 23:34:28 -0300

    Removido modal antigo

--- src/view/cartao/index.js
+++ src/view/cartao/index.js
@@ -76,25 +76,6 @@ function Cartao() {
                 </div>
             </main>
 
-            <div className="modal fade" id="myModal" tabIndex="-1" role="dialog">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title">Exclusão</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">
-                            <p>Tem certeza que deseja remover esse registro?</p>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-primary" id="excluir">Sim</button>
-                            <button type="button" className="btn btn-primary" data-dismiss="modal">Cancelar</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     );
 }
