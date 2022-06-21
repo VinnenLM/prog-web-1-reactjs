91140ab9ff63a6aa06b73866a4581bc44dc7738a
Vinícius de Lima Mendonça <vinicius.mendoncaa@gmail.com>
2022-06-21 20:13:13 -0300

    Refatorado index

--- src/view/index/index.js
+++ src/view/index/index.js
@@ -1,57 +1,71 @@
-import React from "react";
-import EstiloIndex from './index.module.css';
+import React, { useState } from "react";
+import './index.css';
 import SetaClicar from '../../assets/img/seta-clicar.png';
 import Seta from '../../assets/img/seta.png';
 
 function Login() {
-    return(
+
+    const [visivel, setVisivel] = useState('naoVisivel');
+    const [naoVisivel, setNaoVisivel] = useState('');
+
+    const mostrar = () => {
+        if (visivel === 'naoVisivel') {
+            setVisivel('');
+            setNaoVisivel('naoVisivel');
+        } else {
+            setVisivel('naoVisivel');
+            setNaoVisivel('');
+        }
+    }
+
+    return (
         <main>
-        <div className={EstiloIndex.containerPrincipal}>
+            <div className='containerPrincipal'>
 
-            <div>
-                <h1 className={EstiloIndex.titulo}>Mind Booster</h1>
-                <h3 className={EstiloIndex.subTitulo}>Treine a sua memória com Flash Cards!</h3>
-            </div>
+                <div>
+                    <h1 className='titulo'>Mind Booster</h1>
+                    <h3 className='subTitulo'>Treine a sua memória com Flash Cards!</h3>
+                </div>
 
-            <div className={EstiloIndex.containerCartoes}>
+                <div className='containerCartoes'>
 
-                <p className={EstiloIndex.cartaoP}>Como se chama carro em inglês?</p>
-                
-                <div className={EstiloIndex.cartoes}>
-                   
-                    <div id="cartao-clicavel" className={EstiloIndex.divCartao}>
-                        <div className={EstiloIndex.clicar}>
-                            <span>Clique no cartão <br/> para descobrir</span>
-                            <img src={SetaClicar} alt=""/>
-                        </div>
-                        <div className={EstiloIndex.cartao}>
-                            <div className={EstiloIndex.cartaoFrente}>
-                                <span>Carro</span>
+                    <p className={`cartaoP ${naoVisivel}`}>Como se chama carro em inglês?</p>
+
+                    <div className='cartoes'>
+
+                        <div id="cartao-clicavel" className='divCartao' onClick={mostrar}>
+                            <div className={`clicar ${naoVisivel}`}>
+                                <span>Clique no cartão <br /> para descobrir</span>
+                                <img src={SetaClicar} alt="" />
+                            </div>
+                            <div className='cartao'>
+                                <div className='cartaoFrente'>
+                                    <span>Carro</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className={`${EstiloIndex.cartaoSeta} ${EstiloIndex.naoVisivel}`}>
-                        <img className={EstiloIndex.seta} src={Seta} alt=""/>
-                    </div>
+                        <div className={`cartaoSeta ${visivel}`}>
+                            <img className='seta' src={Seta} alt="" />
+                        </div>
 
-                    <div id="cartao-completo" className={`${EstiloIndex.divCartao} ${EstiloIndex.naoVisivel}`}>
-                        <div className={EstiloIndex.cartao}>
-                            <div className={EstiloIndex.cartaoFrente}>
-                                <span>Carro</span>
-                            </div>
-                            <div className={EstiloIndex.separacao}></div>
-                            <div className={EstiloIndex.cartaoVerso}>
-                                <span>Car</span>
+                        <div className={`divCartao ${visivel}`}>
+                            <div className='cartao'>
+                                <div className='cartaoFrente'>
+                                    <span>Carro</span>
+                                </div>
+                                <div className='separacao'></div>
+                                <div className='cartaoVerso'>
+                                    <span>Car</span>
+                                </div>
                             </div>
                         </div>
+
                     </div>
 
                 </div>
-
             </div>
-        </div>
-    </main>
+        </main>
     )
 }
 
